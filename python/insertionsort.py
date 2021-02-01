2d89ce3ad19cd18dfdb6079483cb80cc78d38a0b
Sabatini-1898903 <sabatini.1898903@studenti.uniroma1.it>
2021-02-01 14:50:47 +0100

    dev: Aggiunta InsertionSort

---
+++ python/insertionsort.py
@@ -0,0 +1,8 @@
+def insertion_sort(nums):
+    for i in range(1, len(nums)):
+        item_to_insert = nums[i]
+        j = i - 1
+        while j >= 0 and nums[j] > item_to_insert:
+            nums[j + 1] = nums[j]
+            j -= 1
+        nums[j + 1] = item_to_insert
