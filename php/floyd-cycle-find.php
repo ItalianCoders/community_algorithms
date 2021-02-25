5bfcde7fb303b7b68e0d60d58c5b53c23c4d7b01
Salvatore <s.rotondo90@gmail.com>
2021-02-25 19:01:14 +0100

    dev: add a php version of floyd's cycle find algorithm

---
+++ php/floyd-cycle-find.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Cycle Find Algorithm
+ * 
+ * Given an array of values tells if there's a cycle in it by
+ * using the known Robert W. Floyd's tortoise and hare algorithm
+ * Cycle finding is the algorithmic problem of finding a cycle 
+ * in a sequence of iterated function values. 
+ *
+ * @param array $sequence an indexed array of values
+ * @return bool true if a cycle is detected, false otherwise
+ */
+function floyd_cycle_find(array $sequence): bool 
+{
+    $tortoise = 0;
+    $hare = 0;
+    $length = count($sequence);
+ 
+    while (isset($sequence[++$hare])) {
+        if ($sequence[$tortoise] === $sequence[$hare]) {
+            return true;
+        }
+        
+        ++$tortoise;
+        ++$hare;
+        
+        if ($hare >= $length) {
+            $hare -= $length-1;
+        }
+    }
+}
